04fe21e6016d2e03ce910c9c342e036a98f8a8b1
anikeydev <dev.anikey.va@mail.ru>
2022-11-12 20:42:04 +0300

    lint

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,6 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable import/no-unresolved */
 import readlineSync from 'readline-sync';
 
 const cli = {
